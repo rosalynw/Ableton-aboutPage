78ea215a8b1bed459e422369b11f908471b5a193
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2024-04-15 19:08:04 -0400

    chore: Add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'clementine-orange': '#ff764d',
+        'pale-yellow': '#fbffa7',
+        'pale-blue': '#b1c5ff',
+        'deep-blue': '#0000ff',
+        'lavender': '#d5b3ff',
+        'mint': '#b6ffc0'
+      }
+    },
   },
   plugins: [],
 }
