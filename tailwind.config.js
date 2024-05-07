7034c2f9af8576274e922fd235a9116847b9753f
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2024-05-06 20:49:43 -0400

    Style: Change font spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'clementine-orange': '#ff764d',
+        'pale-yellow': '#fbffa7',
+        'pale-blue': '#b1c5ff',
+        'deep-blue': '#0000ff',
+        'lavender': '#d5b3ff',
+        'mint': '#b6ffc0'
+      },
+      fontFamily: {
+        'josefin-sans': ['"Josefin Sans"', 'sans-serif'],
+      },
+      fontWeight: {
+        'regular': 400,
+        'medium': 500,
+        'bold': 700,
+      },
+
+    },
   },
   plugins: [],
 }
